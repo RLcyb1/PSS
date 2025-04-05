8269716fdfc459b9102fbe8932fac3f46f59dd74
RLcyb1 <wolt5rob@gmail.com>
2025-04-05 15:04:02 +0200

    Delete serwis_samochodowy directory

--- serwis_samochodowy/app/views/auth/login.php
+++
@@ -1,6 +0,0 @@
-<form action="/serwis_samochodowy/public/index.php?action=login" method="POST">
-
-  Login: <input type="text" name="login" required><br>
-  Hasło: <input type="password" name="password" required><br>
-  <button>Zaloguj się</button>
-</form>
--- serwis_samochodowy/app/views/client/new_appointment.php
+++
@@ -1,7 +0,0 @@
-<form action="/serwis_samochodowy/public/index.php?action=addAppointment" method="POST">
-  <label>Data wizyty:</label><br>
-  <input type="datetime-local" name="appointmentDate" required><br>
-  <label>Opis:</label><br>
-  <textarea name="description" required></textarea><br>
-  <button type="submit">Zarezerwuj</button>
-</form>
