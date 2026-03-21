2dd50e85b2ad3007b434dc21fe9c0ad1bf5a94fe
RLcyb1 <wolt5rob@gmail.com>
2026-03-21 17:47:12 +0100

    Delete serwis_samochodowy directory

--- serwis_samochodowy/app/views/client/my_appointments.php
+++
@@ -1,7 +0,0 @@
-<?php foreach ($appointments as $app): ?>
-  <div>
-    Data: <?= $app->appointmentDate ?><br>
-    Status: <?= $app->status ?><br>
-    Opis: <?= $app->description ?><br>
-  </div>
-<?php endforeach; ?>
